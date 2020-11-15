b07fe779f8c013597f090e23b51c31039ab9cd97
kimhaun <wha02068@gmail.com>
2020-11-15 15:42:10 +0900

    피자가게

---
+++ app/src/main/java/kr/co/neoplus/pizzaordertyping_20201115/fragments/PizzaStoreFragment.kt
@@ -0,0 +1,18 @@
+package kr.co.neoplus.pizzaordertyping_20201115.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kr.co.neoplus.pizzaordertyping_20201115.R
+
+class PizzaStoreFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_pizza_store, container, false)
+    }
+}
