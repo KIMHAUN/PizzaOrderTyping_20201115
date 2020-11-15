1c35e621be5d71e3ccfcf35af75cc960f7c0db90
kimhaun <wha02068@gmail.com>
2020-11-15 15:53:34 +0900

    어댑터/뷰페이저 연결 작업

--- app/src/main/java/kr/co/neoplus/pizzaordertyping_20201115/MainActivity.kt
+++ app/src/main/java/kr/co/neoplus/pizzaordertyping_20201115/MainActivity.kt
@@ -2,10 +2,27 @@ package kr.co.neoplus.pizzaordertyping_20201115
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Adapter
+import kotlinx.android.synthetic.main.activity_main.*
+import kr.co.neoplus.pizzaordertyping_20201115.adapters.MyViewPagerAdapter
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
+
+    lateinit var myViewPagerAdapter : MyViewPagerAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupEvents()
+        setValues()
     }
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+        myViewPagerAdapter = MyViewPagerAdapter(supportFragmentManager)
+        myViewPager.adapter = myViewPagerAdapter
+    }
+
+
 }
