8bd26f2152f03f6ed370a0d978b57c575ff256e6
kimhaun <wha02068@gmail.com>
2020-11-21 12:21:53 +0900

    값세팅된 상세 화면으로 넘어감.

--- app/src/main/java/kr/co/neoplus/pizzaordertyping_20201115/StoreDetailActivity.kt
+++ app/src/main/java/kr/co/neoplus/pizzaordertyping_20201115/StoreDetailActivity.kt
@@ -2,6 +2,9 @@ package kr.co.neoplus.pizzaordertyping_20201115
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_store_detail.*
+import kr.co.neoplus.pizzaordertyping_20201115.datas.Store
 
 class StoreDetailActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +17,11 @@ class StoreDetailActivity : BaseActivity() {
     }
 
     override fun setValues() {
+        val pizzaStore = intent.getSerializableExtra("pizzaStore") as Store
+        storeNameTxt.text = pizzaStore.name
+        storeFoneNoTxt.text = pizzaStore.phoneNum
+        Glide.with(mContext).load(pizzaStore.logoURL).into(logoImg)
+
     }
 
 
